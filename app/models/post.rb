d78bec0140deed60bda7ec135e75195179423c7f
Zach Ruiz <powersbooth10@gmail.com>
2022-11-03 19:32:08 -0500

    commit tree 13ca77b9d9e2a274122b210a6c71e43ed2c6c326

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in:  ['Fiction', 'Non-Fiction'] }
 end
